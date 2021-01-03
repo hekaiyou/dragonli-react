9d1d8237defc7d89275df32fddc424d56fd82cb1
hekaiyou <hekaiyou@qq.com>
2021-01-04 01:04:53 +0800

    2020/1/4 1:04

--- src/pages/Synthesis.js
+++ src/pages/Synthesis.js
@@ -1,31 +1,63 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-import SynthesisLanguageSelect from '../components/synthesis/SynthesisLanguageSelect.js';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 
 class Synthesis extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            language: 'en-us',
+            text: '',
+            history: ['sj'],
         };
     }
 
-    handleLanguageSelect(i) {
-        const newLanguage = i.target.value;
-        if (newLanguage) {
+    handleTextFieldChange(e) {
+        this.setState({
+            text: e.target.value,
+        })
+    }
+
+    handleButtonClick() {
+        const text = this.state.text;
+        if (text.trim() !== '') {
+            const history = this.state.history;
+            history.push(text);
             this.setState({
-                language: newLanguage,
-            });
+                history: history,
+            })
         }
     }
 
     render() {
+        const textAndAudio = this.state.history.forEach((item) => {
+            return (
+                <Typography>
+                    {item}
+                    <audio controls autoPlay src="http://0.0.0.0:5000/?text={item}" type="audio/wav">
+                        Your browser does not support the audio element.
+                    </audio>
+                </Typography>
+            );
+        });
+
         return (
             <div>
                 <form noValidate autoComplete="off">
-                    <TextField id="tts-key" label="TTS Key (Englist)" fullWidth />
+                    <TextField id="tts-text" onChange={(e) => this.handleTextFieldChange(e)} label="TTS Text" multiline rows={3} fullWidth />
                     <p />
-                    <SynthesisLanguageSelect language={this.state.language} onClick={i => this.handleLanguageSelect(i)} />
+                    <Button variant="contained" color="primary" onClick={() => this.handleButtonClick()} fullWidth>
+                        Text To Speech
+                    </Button>
+                    <p />
+                    <Card>
+                        <CardContent>
+                            {textAndAudio}
+                        </CardContent>
+                    </Card>
                 </form>
             </div>
         );
